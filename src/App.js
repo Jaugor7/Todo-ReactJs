b99a1b73de46044a475604c7a92f73ce337d76a2
Jaugor7 <sudhansh0706@gmail.com>
2021-02-25 00:33:52 +0530

    First Day Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Takes Dogs for a Walk.',"Learning with Live Stream." ,'Take Rubbish Out.']); //todos list will startt with empty state.
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h1> Hello World.....! {1+1}</h1>
+        <form>
+          <input value={input} onChange={event => setInput(event.target.value )}/>
+          <button type="submit" onClick={addTodo}>Add Todo</button>
+        </form>
+        <ul>
+          {todos.map(todo => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+
+
+
     </div>
   );
 }
